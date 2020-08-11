00e116f2b8dad817b99c36cab2f34a5940f91d66
Philip Hamner <philiphamner@gmail.com>
2020-08-11 14:35:27 -0700

    app skeleton built.

---
+++ client/Components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const Map = function() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Map Goes Here</Text>
+      </View>
+    );
+}
+
+export default Map;
---
+++ client/Components/MyTabBar.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const MyTabBar = function({ state, descriptors, navigation }) {
+  return (
+    <View style={{ 
+        flexDirection: 'row', 
+        height: 50, 
+        backgroundColor: 'powderblue' 
+        }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityStates={isFocused ? ['selected'] : []}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+export default MyTabBar;
