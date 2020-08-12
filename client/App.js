d22c0e34f6540b00314d5966ad82f602bf889809
Philip Hamner <philiphamner@gmail.com>
2020-08-12 14:37:40 -0700

    Add icons as buttons in footer.

--- client/App.js
+++ client/App.js
@@ -5,6 +5,7 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Icon } from 'react-native-elements';
 
 import AllEvents from './Components/AllEvents';
 import Calendar from './Components/Calendar';
@@ -28,46 +29,96 @@ let HomeScreen = function({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button
+      <Icon name="md-beer" type="ionicon" color="#887700" />
+
+      {/* <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+        onPress={() => navigation.navigate('Details', {
+          itemId: 86,
+          otherParam: 'anything you want here',
+        })}
+      /> */}
 
     </View>
   );
 }
 
-let DetailsScreen = function({ navigation }) {
+
+let DetailsScreen = function({ route, navigation }) {
+  /* 2. Get the param */
+  // const { itemId } = route.params;
+  // const { otherParam } = route.params;
+  
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
+      {/* <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text> */}
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} /> */}
+
+
 
     </View>
   );
 }
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+// initialRouteName="Home"
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
+      <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-map' : 'md-map';
+            } else if (route.name === 'Details') {
+              iconName = focused ? 'ios-list' : 'ios-list';
+            }
+
+            return <Icon name={iconName} type="ionicon" color="red" />
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Details" component={DetailsScreen} />
+      </Tab.Navigator>
+
+
+
+      {/* <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#0079d3',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+      > */}
+        {/* <Stack.Screen 
           name="Home" 
           component={HomeScreen}  
-          options={{ title: 'Overview' }}
+          options={{title: 'My Home'}}
         />
         <Stack.Screen 
           name="Details" 
           component={DetailsScreen} 
           options={{ title: 'All the Deets' }}
-        />
+        /> */}
 
-      </Stack.Navigator>
+      {/* </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
