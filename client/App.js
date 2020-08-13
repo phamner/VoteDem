b3cecb4f1845958ca40f6a7073ade3c8766eae0e
Philip Hamner <philiphamner@gmail.com>
2020-08-12 17:33:48 -0700

    Convert App to stateful component.

--- client/App.js
+++ client/App.js
@@ -56,59 +56,65 @@ let DetailsScreen = function({ route, navigation }) {
 }
 
 
-// initialRouteName="Home"
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      testing: []
+    }
+  }
+  render() {
+    return (
+      <NavigationContainer>
+        
 
-function App() {
-  return (
-    <NavigationContainer>
-      
+          <Tab.Navigator 
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-        <Tab.Navigator 
-          initialRouteName="Home"
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+                if (route.name === 'Home') {
+                  iconName = focused ? 'ios-map' : 'md-map';
+                } else if (route.name === 'Details') {
+                  iconName = focused ? 'ios-list-box' : 'ios-list';
+                } else if (route.name === 'Calendar') {
+                  iconName = focused ? 'md-calendar' : 'ios-calendar';
+                }
 
-              if (route.name === 'Home') {
-                iconName = focused ? 'ios-map' : 'md-map';
-              } else if (route.name === 'Details') {
-                iconName = focused ? 'ios-list-box' : 'ios-list';
-              } else if (route.name === 'Calendar') {
-                iconName = focused ? 'md-calendar' : 'ios-calendar';
-              }
+                return <Icon name={iconName} type="ionicon" color="#00aef3" />
+              },
+            })}
 
-              return <Icon name={iconName} type="ionicon" color="#00aef3" />
-            },
-          })}
+            tabBarOptions={{
+              activeTintColor: '#00aef3',
+              inactiveTintColor: 'gray',
+            }}
+          >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Details" component={DetailsScreen} />
+            <Tab.Screen name="Calendar" component={CalendarScreen} />
 
-          tabBarOptions={{
-            activeTintColor: '#00aef3',
-            inactiveTintColor: 'gray',
-          }}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Details" component={DetailsScreen} />
-          <Tab.Screen name="Calendar" component={CalendarScreen} />
-
-        </Tab.Navigator>
+          </Tab.Navigator>
 
 
 
 
-        {/* <Stack.Screen 
-          name="Home" 
-          component={HomeScreen}  
-          options={{title: 'My Home'}}
-        />
-        <Stack.Screen 
-          name="Details" 
-          component={DetailsScreen} 
-          options={{ title: 'All the Deets' }}
-        /> */}
+          {/* <Stack.Screen 
+            name="Home" 
+            component={HomeScreen}  
+            options={{title: 'My Home'}}
+          />
+          <Stack.Screen 
+            name="Details" 
+            component={DetailsScreen} 
+            options={{ title: 'All the Deets' }}
+          /> */}
 
-      {/* </Stack.Navigator> */}
-    </NavigationContainer>
-  );
+        {/* </Stack.Navigator> */}
+      </NavigationContainer>
+    );
+  }
 }
 
 export default App;
